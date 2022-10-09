5162edab9622c39a9ed01d94ef9e6c22b35d0602
Kusmanov <b.kusmanov@gmail.com>
2022-10-09 11:41:34 +0600

    Added class: Dealer

---
+++ src/units/Dealer.java
@@ -0,0 +1,8 @@
+package units;
+
+public class Dealer extends Unit{
+    public Dealer(String name, int health, int agility, int power, int experience) {
+        super(name, 10,  10, 10, 10);
+    }
+
+}
