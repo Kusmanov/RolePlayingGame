c0f1beeda0ef5182017ed62745f9d634e328206b
Kusmanov <b.kusmanov@gmail.com>
2022-10-16 18:41:24 +0600

    Final commit 2

--- src/units/Skeleton.java
+++ src/units/Skeleton.java
@@ -2,7 +2,7 @@ package units;
 
 public class Skeleton extends Unit {
     public Skeleton(String name, int multiplier) {
-        super(name, 30, 10, 5, 100, "Монстр");
+        super(name, 40, 10, 10, 60, "Монстр");
         this.changeHealth(-this.getHealth() + this.getHealth() * multiplier);
         this.changeAgility(-this.getAgility() + this.getAgility() * multiplier);
         this.changePower(-this.getPower() + this.getPower() * multiplier);
