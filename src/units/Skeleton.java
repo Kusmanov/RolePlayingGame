4ce4810e295942f9861e12db26e8282205018816
Kusmanov <b.kusmanov@gmail.com>
2022-10-16 19:17:47 +0600

    Final commit 7 - set balance

--- src/units/Skeleton.java
+++ src/units/Skeleton.java
@@ -2,7 +2,7 @@ package units;
 
 public class Skeleton extends Unit {
     public Skeleton(String name, int multiplier) {
-        super(name, 10, 10, 15, 100, "Монстр");
+        super(name, 10, 10, 10, 100, "Монстр");
         this.changeHealth(-this.getHealth() + this.getHealth() * multiplier);
         this.changeAgility(-this.getAgility() + this.getAgility() * multiplier);
         this.changePower(-this.getPower() + this.getPower() * multiplier);
